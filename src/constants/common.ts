10f36b2c3584b5ead32e5544bd93d9e7f13522e8
Sumayao03BSIT <sumayaoclyderamerc@gmail.com>
2024-09-06 08:35:42 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Clyde Ramer C. Sumayao';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'no in-job experiences';
+export const ABOUT_TEXT_2 =
+  'no in-job experiences';
+export const ABOUT_TEXT_3 =
+  'no in-job experiences';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'no in-job experiences';
+export const RESUME_TEXT_2 =
+  'no in-job experiences';
